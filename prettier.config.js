b36e813362be9516b778ac1288515f13a73c03fe
Peter Steinberg <bklynpeter@gmail.com>
2025-02-01 12:26:15 -0500

    first remote commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,5 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
+const prettierConfig = {
   plugins: ["prettier-plugin-tailwindcss"],
 };
+export default prettierConfig;
